88b1dc83bdd85301c0b4aafbc711a72bfa400cf5
InfinityRaider <the_xaf@hotmail.com>
2016-08-04 22:43:18 +0200

    Change texture indices

--- src/main/java/com/teaminfinity/elementalinvocations/reference/Reference.java
+++ src/main/java/com/teaminfinity/elementalinvocations/reference/Reference.java
@@ -9,13 +9,14 @@ package com.teaminfinity.elementalinvocations.reference;
 public interface Reference {
 
 	String MOD_ID = /*^${mod.id}^*/ "elemental_invocations";
-	String MOD_NAME = /*^${mod.name}^*/ "Default Mod";
-	String MOD_AUTHOR = /*^${mod.author}^*/ "Captain Nemo";
+
+	String MOD_NAME = /*^${mod.name}^*/ "Elemental Invocations";
+	String MOD_AUTHOR = /*^${mod.author}^*/ "Team Infinity";
 
 	String MOD_VERSION_MAJOR = /*^${mod.version_major}^*/ "0";
 	String MOD_VERSION_MINOR = /*^${mod.version_minor}^*/ "0";
-	String MOD_VERSION_PATCH = /*^${mod.version_patch}^*/ "0";
-	String MOD_VERSION = /*^${mod.version}^*/ "0.0.0";
+	String MOD_VERSION_PATCH = /*^${mod.version_patch}^*/ "1";
+	String MOD_VERSION = /*^${mod.version}^*/ "0.0.1";
 	
 	String MOD_UPDATE_URL = /*^${mod.update_url}^*/ "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 	
