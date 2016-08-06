c32b2a3cc5835c899a28356a5846451bcdcc0a98
InfinityRaider <the_xaf@hotmail.com>
2016-08-06 02:11:16 +0200

    Ball lightning animation

--- src/main/java/com/teaminfinity/elementalinvocations/render/entity/RenderEntityBallLightning.java
+++ src/main/java/com/teaminfinity/elementalinvocations/render/entity/RenderEntityBallLightning.java
@@ -5,6 +5,7 @@ import com.teaminfinity.elementalinvocations.entity.EntityBallLightning;
 import com.teaminfinity.elementalinvocations.reference.Constants;
 import com.teaminfinity.elementalinvocations.reference.Reference;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.VertexBuffer;
 import net.minecraft.client.renderer.entity.RenderManager;
@@ -21,12 +22,29 @@ import java.util.List;
 public class RenderEntityBallLightning extends RenderEntityFlatTexture<EntityBallLightning> {
     private static final ResourceLocation TEXTURE = new ResourceLocation(Reference.MOD_ID.toLowerCase(), "textures/entities/ball_lightning.png");
 
+    private static final int FRAMES = 7;
+    private static final int FRAME_TIME = 2;
+
+    private float lastPartialTick = -1;
+    private int counter = 0;
+    private int frame;
+
     public RenderEntityBallLightning(RenderManager renderManager) {
         super(renderManager);
     }
 
     @Override
     public void doRender(EntityBallLightning e, double x, double y, double z, float entityYaw, float partialTicks) {
+        if(lastPartialTick < 0) {
+            frame = 0;
+        } else {
+            if(partialTicks <= lastPartialTick) {
+                counter = (counter + 1) % FRAME_TIME;
+                frame = counter == 0 ? (frame + 1) % FRAMES : frame;
+            }
+        }
+        lastPartialTick = partialTicks;
+
         List<Entity> mounted = e.getPassengers();
         if(mounted.size() > 0 && mounted.get(0) == ElementalInvocations.proxy.getClientPlayer() && Minecraft.getMinecraft().gameSettings.thirdPersonView == 0) {
             //don't render the entity in first person
@@ -39,12 +57,23 @@ public class RenderEntityBallLightning extends RenderEntityFlatTexture<EntityBal
     protected void renderTexture(EntityBallLightning entity, VertexBuffer buffer, Tessellator tessellator) {
         float u = Constants.UNIT;
 
+        GlStateManager.disableLighting();
+        GlStateManager.enableBlend();
+        GlStateManager.enableAlpha();
+        GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GlStateManager.alphaFunc(GL11.GL_GREATER, 0.05F);
+
+        GlStateManager.color(255, 255, 255, 127);
+
         buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
 
-        buffer.pos(-8 * u, 0, 0).tex(1, 1).endVertex();
-        buffer.pos(-8 * u, 16 * u, 0).tex(1, 0).endVertex();
-        buffer.pos(8 * u, 16 * u, 0).tex(0, 0).endVertex();
-        buffer.pos(8 * u, 0, 0).tex(0, 1).endVertex();
+        double minV = (frame) * (1.0/FRAMES);
+        double maxV = (frame + 1) * (1.0/FRAMES);
+
+        buffer.pos(-12 * u, 0, 0).tex(1, maxV).endVertex();
+        buffer.pos(-12 * u, 24 * u, 0).tex(1, minV).endVertex();
+        buffer.pos(12 * u, 24 * u, 0).tex(0, minV).endVertex();
+        buffer.pos(12 * u, 0, 0).tex(0, maxV).endVertex();
 
         tessellator.draw();
     }
