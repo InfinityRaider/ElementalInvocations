aeeb285f58093aa3f1170138626ff10dedd7ca49
InfinityRaider <the_xaf@hotmail.com>
2018-02-11 21:53:53 +0100

    Config saving

--- src/main/java/com/infinityraider/elementalinvocations/config/ModConfigGui.java
+++ src/main/java/com/infinityraider/elementalinvocations/config/ModConfigGui.java
@@ -1,12 +1,11 @@
 package com.infinityraider.elementalinvocations.config;
 
+import com.infinityraider.elementalinvocations.ElementalInvocations;
 import com.infinityraider.elementalinvocations.reference.Reference;
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraftforge.common.config.ConfigElement;
 import net.minecraftforge.fml.client.config.GuiConfig;
 import net.minecraftforge.fml.client.config.IConfigElement;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ModConfigGui extends GuiConfig {
@@ -16,8 +15,6 @@ public class ModConfigGui extends GuiConfig {
     }
 
     private static List<IConfigElement> getConfigElements() {
-        List<IConfigElement> elements = new ArrayList<>();
-        ModConfiguration.getInstance().getCategories().forEach(category -> elements.addAll(new ConfigElement(category).getChildElements()));
-        return elements;
+        return ElementalInvocations.instance.getConfigurationHandler().getConfigElements();
     }
 }
