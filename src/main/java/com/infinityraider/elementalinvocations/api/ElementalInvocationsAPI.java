694087ccfba73028ae0dbeb49d2c89fa9d70dc92
InfinityRaider <the_xaf@hotmail.com>
2018-02-11 19:22:46 +0100

    API reduncancy cleanup

--- src/main/java/com/infinityraider/elementalinvocations/api/ElementalInvocationsAPI.java
+++ src/main/java/com/infinityraider/elementalinvocations/api/ElementalInvocationsAPI.java
@@ -1,41 +1,11 @@
 package com.infinityraider.elementalinvocations.api;
 
-import com.infinityraider.elementalinvocations.api.souls.ISoulCollection;
-import com.infinityraider.elementalinvocations.api.spells.ISpellBuilder;
-import com.infinityraider.elementalinvocations.api.spells.ISpellRegistry;
-import net.minecraftforge.common.capabilities.Capability;
-
 import java.util.Optional;
 
-public class ElementalInvocationsAPI implements IElementalInvocationsAPI {
+public class ElementalInvocationsAPI {
     private static IElementalInvocationsAPI API;
 
     public static Optional<IElementalInvocationsAPI> getApi() {
         return Optional.ofNullable(API);
-    }
-
-    @Override
-    public Capability<? extends IPlayerMagicProperties> magicPropertiesCapability() {
-        return API.magicPropertiesCapability();
-    }
-
-    @Override
-    public Capability<? extends ISoulCollection> soulCollectionCapability() {
-        return API.soulCollectionCapability();
-    }
-
-    @Override
-    public ISpellRegistry getSpellRegistry() {
-        return API.getSpellRegistry();
-    }
-
-    @Override
-    public ISpellBuilder getNewSpellBuilder() {
-        return API.getNewSpellBuilder();
-    }
-
-    @Override
-    public IMagicDamageHandler getDamageHandler() {
-        return API.getDamageHandler();
     }
 }
